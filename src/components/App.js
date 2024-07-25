67f8552f5028b72039fc649be8c1cc7a87b3aea3
Sakshi <sakshichoudhary000@gmail.com>
2024-07-26 01:34:35 +0530

    try1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,66 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
+import axios from "axios";
+import "regenerator-runtime/runtime";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [city, setCity] = useState("mumbai");
+  const [input, setInput] = useState("");
+  const [weather, setWeather] = useState({});
+  const [error, setError] = useState("");
 
-export default App
+  const apiKey = "4deb9dcaf8bafa8ecfe2be9e9c7d02ca";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  const fetchApi = async () => {
+    try {
+      const response = await axios.get(url);
+       //console.log(response.data);
+      setWeather(response.data);
+      setError("");
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+      setError("Error fetching data. Please try again.");
+      setWeather({});
+    }
+  };
+
+  useEffect(() => {
+    fetchApi();
+    console.log("called fetchAPI");
+    return () => console.log("cleanup");
+  }, [city]);
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const display = () => {
+    setCity(input);
+  };
+
+  return (
+    <div className="main" id="main">
+      <div className="search">
+        <input type="text" value={input} onChange={handleChange} />
+        <button onClick={display}>Search</button>
+      </div>
+      <div className="weather">
+        {error && <h2>{error}</h2>}
+        {!error && weather.main && (
+          <>
+            <h1>{city}</h1>
+            <h2>{weather.main.temp}°F</h2>
+            <h3>{weather.weather[0].description}</h3>
+            <img
+              src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+              alt="cloud image"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
